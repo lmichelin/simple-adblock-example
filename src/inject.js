6b96dce5042e20d3267b7cfdb47b18d44ac37368
Louis-Marie Michelin <lmichelin@outlook.fr>
2019-08-29 00:06:04 +0200

    use MutationObserver to block dynamic cookie banner

--- src/inject.js
+++ src/inject.js
@@ -7,3 +7,19 @@ for (const selector of selectors) {
     document.querySelector(selector).style.display = "none"
   } catch (error) {}
 }
+
+const observer = new MutationObserver(mutationsList => {
+  for (const mutation of mutationsList) {
+    if (mutation.type !== "childList") continue
+
+    // A child node has been added or removed
+
+    for (const addedNode of mutation.addedNodes) {
+      if (addedNode.attributes && addedNode.getAttribute("aria-label") === "cookieconsent") {
+        addedNode.style.display = "none"
+      }
+    }
+  }
+})
+
+observer.observe(document.body, { childList: true, subtree: true })
